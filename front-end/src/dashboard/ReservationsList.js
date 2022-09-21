4ebb13837dcbe5f92450a070db5b1e95815a406f
Lev_Gordon <lev.gordonsa@gmail.com>
2022-09-20 20:43:11 -0700

    ReservationsList complete

--- front-end/src/dashboard/ReservationsList.js
+++ front-end/src/dashboard/ReservationsList.js
@@ -14,34 +14,43 @@ function ReservationList({ reservation, formatTime }) {
     people,
     reservation_id,
   } = reservation;
-  
+
   let formattedTime = formatTime(reservation_time);
-  let formattedHours = Number(formattedTime.slice(0,2)) > 12 ? Number(formattedTime.slice(0,2) % 12) : Number(formattedTime.slice(0,2));
+  let formattedHours =
+    Number(formattedTime.slice(0, 2)) > 12
+      ? Number(formattedTime.slice(0, 2) % 12)
+      : Number(formattedTime.slice(0, 2));
   formattedTime = `${formattedHours}${formattedTime.slice(2)}`;
 
   const cancelHandler = async (e) => {
-    if (window.confirm("Do you want to cancel this reservation? This cannot be undone.")) {
-        const response = await fetch(`${REACT_APP_API_BASE_URL}/reservations/${reservation_id}/status`, {
-            method: "PUT",
-            headers: {
-              "Content-type": "application/json",
-            },
-            body: JSON.stringify({data: { status: "cancelled" } }),
-            
-          }, history.go(0));
-          return response;
+    if (
+      window.confirm(
+        "Do you want to cancel this reservation? This cannot be undone."
+      )
+    ) {
+      const response = await fetch(
+        `${REACT_APP_API_BASE_URL}/reservations/${reservation_id}/status`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-type": "application/json",
+          },
+          body: JSON.stringify({ data: { status: "cancelled" } }),
+        },
+        history.go(0)
+      );
+      return response;
     }
-  }
+  };
 
-
-  
   const reservationCard = (
     <div id="reservation-card">
       <div id="card-title">
         Reservation for {formattedTime}{" "}
         {new Date(`${reservation_date} ${reservation_time}`).getHours() < 12
           ? "AM"
-          : "PM"} for {reservation_date.slice(0, 10)}
+          : "PM"}
+        <div>{reservation_date.slice(0, 10)}</div>
       </div>
       <div>
         <h6 id="card-label">Name:</h6>
@@ -49,26 +58,53 @@ function ReservationList({ reservation, formatTime }) {
           {first_name} {last_name}
         </p>
         <h6 id="card-label">Status:</h6>
-        <p id="card-text" data-reservation-id-status={reservation_id}>{status}</p>
+        <p id="card-text" data-reservation-id-status={reservation_id}>
+          {status}
+        </p>
         <h6 id="card-label">Contact Number:</h6>
         <p id="card-text">{mobile_number}</p>
         <h6 id="card-label">Number of Guests:</h6>
         <p id="card-text">{people}</p>
       </div>
-      <div className="d-flex justify-content-end">
-      {status === "Booked" ? <button type="button"  className="colorfulBtn" onClick={() => history.push(`/reservations/${reservation_id}/seat`)}>Seat</button> : null}
-        {status === "Booked" ? <button type="button" className="colorfulBtn" onClick={() => history.push(`/reservations/${reservation_id}/edit`)}>Edit </button> : null}
-        {status === "Booked" ? <button type="button" className="colorfulBtn" data-reservation-id-cancel={reservation.reservation_id} onClick={cancelHandler}> <div> Cancel </div> </button> : null}
+      <div className="d-flex justify-content-center">
+        {status === "Booked" ? (
+          <button
+            type="button"
+            className="reservations-buttons"
+            onClick={() => history.push(`/reservations/${reservation_id}/seat`)}
+          >
+            Seat
+          </button>
+        ) : null}
+        {status === "Booked" ? (
+          <button
+            type="button"
+            className="reservations-buttons"
+            onClick={() => history.push(`/reservations/${reservation_id}/edit`)}
+          >
+            Edit{" "}
+          </button>
+        ) : null}
+        {status === "Booked" ? (
+          <button
+            type="button"
+            className="reservations-buttons"
+            data-reservation-id-cancel={reservation.reservation_id}
+            onClick={cancelHandler}
+          >
+            {" "}
+            <div> Cancel </div>{" "}
+          </button>
+        ) : null}
       </div>
     </div>
   );
 
-
-
   if (reservation.status !== "cancelled") {
-    return (
-      reservationCard
-    )} else {return null}  
+    return reservationCard;
+  } else {
+    return null;
+  }
 }
 
 export default ReservationList;
