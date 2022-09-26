909e147701b13f7c9d204e2e088a4aedfec6ed1e
Lev_Gordon <lev.gordonsa@gmail.com>
2022-09-26 16:16:39 -0700

    heroku deployment

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -6,4 +6,4 @@ router.route("/").get(controller.list).post(controller.create).all(methodNotAllo
 router.route("/:reservation_id").get(controller.read).put(controller.updateReservation).all(methodNotAllowed)
 router.route("/:reservation_id/status").put(controller.update).all(methodNotAllowed);
 
-module.exports = router;
+module.exports = router; 
